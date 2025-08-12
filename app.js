ca0341556d0a6b6d7a413afefec57f08a2570a61
SAMARA ROLIM DOS SANTOS <25132563@CFP127ALUNO.LOCAL>
2025-08-12 11:13:17 -0300

    atividade de funcionarios

---
+++ app.js
@@ -0,0 +1,22 @@
+'use strict'
+
+import funcionarios from "./funcionarios/funcionarios.json" with {type: json}
+
+function criarImagem (srcImagens){
+    const galeria = document.getElementById('galeria')
+    const imagem = document.createElement('img')
+    
+    imagem.scr = srcImagens.imagem
+
+    galeria.appendChild(imagem)
+}
+
+function carregarImagem (){
+
+    const galeria = document.getElementById('galeria')
+
+    imagens.forEach(criarImagem)
+
+}
+
+carregarImagem ()
